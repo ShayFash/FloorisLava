2264a883f42790377ef839eda7d4cb89f61234ab
tourist339 <kartikkapoor3390@gmail.com>
2021-11-07 17:01:18 -0600

    Objects spawned with random x , y coordinates within specified bounds

--- Assets/ProceduralManager.cs
+++ Assets/ProceduralManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vector2 = UnityEngine.Vector2;
 
 public class ProceduralManager : MonoBehaviour
 {
@@ -15,10 +14,14 @@ public class ProceduralManager : MonoBehaviour
     public float playerMaxY;
 
     public float playerMaxX;
+    public Transform RightBound, LeftBound;
+
+    private float rightBoundX, leftBoundX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rightBoundX = RightBound.position.x;
+        leftBoundX = LeftBound.position.x;
     }
 
     // Update is called once per frame
@@ -29,9 +32,29 @@ public class ProceduralManager : MonoBehaviour
         Vector2 lastSpawnedObjectPosition = lastSpawnedObject.transform.position;
         if (lastSpawnedObject.transform.position.y < spawnThreshold.position.y)
         {
-            Vector3 newObjectTransform = new Vector3(lastSpawnedObjectPosition.x,
-                lastSpawnedObjectPosition.y + Random.Range(5, playerMaxY),0);
+
+            int randomDirection = Random.Range(0, 2) * 2 - 1;
+                
+            float newObjectY = lastSpawnedObjectPosition.y + Random.Range(2, playerMaxY);
+            float newObjectX = lastSpawnedObjectPosition.x + randomDirection* Random.Range(5, playerMaxX);
+            
+            Vector3 newObjectTransform = new Vector3(newObjectX,newObjectY,0);
+            
             SpawnableObject newObject=Instantiate(simplePlatform, newObjectTransform, Quaternion.identity);
+            Debug.Log("X: "+newObjectTransform.x+" EndX: "+newObject.EndX+" BoundX: "+rightBoundX);
+
+            if (newObject.StartX<leftBoundX || newObject.EndX>rightBoundX)
+            {
+                Destroy(newObject.gameObject);
+                Debug.Log("destroyed"+newObject);
+                randomDirection *= -1;
+                newObjectX = lastSpawnedObjectPosition.x + randomDirection* Random.Range(2, playerMaxX);
+                newObjectTransform = new Vector3(newObjectX,newObjectY,0);
+                newObject=Instantiate(simplePlatform, newObjectTransform, Quaternion.identity);
+                Debug.Log("New Object X: "+newObjectTransform.x+" EndX: "+newObject.EndX+" BoundX: "+rightBoundX);
+
+                
+            }
             spawnedObjects.Add(newObject);
         }
         

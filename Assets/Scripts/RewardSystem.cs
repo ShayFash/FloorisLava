68ceef2781db9daebba703c73f0a006760fe314a
tourist339 <kartikkapoor3390@gmail.com>
2021-12-06 23:50:24 -0600

    update

--- Assets/Scripts/RewardSystem.cs
+++ Assets/Scripts/RewardSystem.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.Mathematics;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RewardSystem : MonoBehaviour
 {
 
     public GameObject player;
     public GameObject Coin;
-    [SerializeField] private GameObject glory;
-    public static int HEALTH_POTION_COINS = 300;
-    public static int RAGE_MODE_COINS = 600;
 
+    public static int HEALTH_POTION_COINS = 300;
     public static int SNOW_POTION_COINS = 400;
 
+    public static int RAGE_MODE_COINS = 600;
+
+    
     private float playerHeight;
     private int totalKills;
     private int totalPlayerKills=0;
@@ -24,7 +21,6 @@ public class RewardSystem : MonoBehaviour
     public int killsForRageMode=20;
     public ScoreBar scoreBar;
     private int score;
-    // private bool check;
 
     public int Score
     {
@@ -43,8 +39,6 @@ public class RewardSystem : MonoBehaviour
         score = 0;
         playerHeight = 0;
         Score = 0;
-        // check = true;
-        Glory();
     }
 
     // Update is called once per frame
@@ -52,8 +46,6 @@ public class RewardSystem : MonoBehaviour
     {
         playerHeight = player.transform.position.y;
         calculateScore();
-        Glory();
-        
     }
 
     private void calculateScore()
@@ -62,14 +54,12 @@ public class RewardSystem : MonoBehaviour
         if (updatedScore > Score)
         {
             Score = updatedScore;
-            
         }
     }
 
     public void increaseKill()
     {
         this.totalPlayerKills++;
-        this.totalKills++;
         if (totalPlayerKills % 3 == 0)
         {
             PlayerAccess.getStats().addRageMode();
@@ -81,46 +71,6 @@ public class RewardSystem : MonoBehaviour
 
         }
     }
-    public void Glory()
-    {
-        glory = GameObject.FindWithTag("glory");
-        player= GameObject.FindWithTag("Player");
-        Debug.Log(totalKills);
-        if (totalKills>0)
-        {        Debug.Log("total");
 
-            glory.GetComponent<Text>().enabled = true;
-            
-            if (!PlayerAccess.getMovement().LookingRight)
-            {
-                Helper.rotate180(glory.transform);
-            }
-            else
-            {
-                Helper.rotate180(glory.transform);
-            }
-                
-            Invoke("Hide",5f);
-            
-                
-        }
-        else
-        {
-            glory.GetComponent<Text>().enabled = false;
 
-        }
-            
-        
-        
-    }
-
-    private void Hide()
-    {
-        glory.GetComponent<Text>().enabled = false;
-        totalKills = 0;
-
-    }
-    
 }
-
-
